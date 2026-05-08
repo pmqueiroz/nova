21b9c46edad22630a9fc0f8a48eb2fe7266de870
pmqueiroz <pedroqueiroz593@gmail.com>
2026-05-07 21:06:12 -0300

    fix: default font name

--- src/ui/theme/font.rs
+++ src/ui/theme/font.rs
@@ -1,12 +1,12 @@
 pub const REGULAR: iced::Font = iced::Font {
-  family: iced::font::Family::Name("Fira Code"),
+  family: iced::font::Family::Name("FiraCode Nerd Font"),
   weight: iced::font::Weight::Normal,
   stretch: iced::font::Stretch::Normal,
   style: iced::font::Style::Normal,
 };
 
 pub const BOLD: iced::Font = iced::Font {
-  family: iced::font::Family::Name("Fira Code"),
+  family: iced::font::Family::Name("FiraCode Nerd Font"),
   weight: iced::font::Weight::Bold,
   stretch: iced::font::Stretch::Normal,
   style: iced::font::Style::Normal,

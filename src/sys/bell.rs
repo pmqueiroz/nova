e8e63e03d511c2c6762f17efee865e4be3d2ccca
pmqueiroz <pedroqueiroz593@gmail.com>
2026-05-09 09:58:16 -0300

    style: format code

--- src/sys/bell.rs
+++ src/sys/bell.rs
@@ -3,13 +3,12 @@ const BELL_FLAC: &[u8] = include_bytes!("../../assets/sounds/bell.flac");
 
 pub fn play() {
   std::thread::spawn(|| {
-    if let Ok((_stream, handle)) = rodio::OutputStream::try_default() {
-      if let Ok(source) = rodio::Decoder::new(std::io::Cursor::new(BELL_FLAC)) {
-        if let Ok(sink) = rodio::Sink::try_new(&handle) {
-          sink.append(source);
-          sink.sleep_until_end();
-        }
-      }
+    if let Ok((_stream, handle)) = rodio::OutputStream::try_default()
+      && let Ok(source) = rodio::Decoder::new(std::io::Cursor::new(BELL_FLAC))
+      && let Ok(sink) = rodio::Sink::try_new(&handle)
+    {
+      sink.append(source);
+      sink.sleep_until_end();
     }
   });
 }
